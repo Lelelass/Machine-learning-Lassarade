c32f837d10929ab0a1d873f76e0175a4d3b65bd1
Lelelass <82147263+Lelelass@users.noreply.github.com>
2022-03-25 10:25:19 +0100

    Minor code arangements

--- labb_1/production_model.py
+++ labb_1/production_model.py
@@ -5,16 +5,15 @@ from sklearn.metrics import classification_report, confusion_matrix, ConfusionMa
 
 model = joblib.load('cardio-classification-model.pkl')
 soft_model = joblib.load('cardio-classification-model-soft-voting.pkl')#For being able to run predict proba (#AttributeError: predict_proba is not available when voting='hard')
+models = [model, soft_model]
 
 data = pd.read_csv('./data/samples.csv', index_col=['id'])
 X, y = data.drop('cardio', axis = 1), data['cardio']
 
 y_pred = model.predict(X)
+y_pred_soft = soft_model.predict(X)
 
 print(classification_report(y, y_pred))
-
-
-y_pred_soft = soft_model.predict(X)
 print(classification_report(y, y_pred_soft))
 
 probabilities = soft_model.predict_proba(X)
